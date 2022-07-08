19afe13bf94f4b1856d8f98877cc28f71fb9970a
nohli <43643339+nohli@users.noreply.github.com>
2022-07-09 00:57:05 +0200

    Remove unused import

--- lib/src/internals.dart
+++ lib/src/internals.dart
@@ -15,8 +15,6 @@
  * limitations under the License.
  */
 
-import 'dart:ui';
-
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:xml/xml_events.dart';
