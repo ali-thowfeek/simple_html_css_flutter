7ec2bdd9686ec32ffd942fa85b79501d575ae98c
Ali Ahamed Thowfeek <alitheoneman@gmail.com>
2024-05-19 14:34:38 +0530

    update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:simple_html_css/simple_html_css.dart';
 
-void main() => runApp(Home());
+void main() => runApp(const Home());
 
 class Home extends StatelessWidget {
+  const Home({super.key});
+
   @override
   Widget build(BuildContext context) {
     const String htmlContent = """
@@ -16,6 +18,7 @@ class Home extends StatelessWidget {
 <p>We all spell <span style='color:crimson; text-decoration: underline wavy;'>recieve</span> wrong.<br />Some times we delete <del>stuff</del></p>
 <div style='font-size:17px'>We write things that are <span style='font-size:1.5em;'>Big,</span> <b>bold</b>&nbsp; or <span style='color:brown'>colorful</span></div>
 <p style='font-family:times;'>Some different FONT with <span style='background-color:lightcyan;'>this part highlighted</span></p>
+<p>x &lt; 2 &amp;&amp;&nbsp;x &gt; 0</p>
 <div style='line-height:2; font-size:17px'><b style='color: rgb(0,122,255); font-weight:500;'>Finally some line heights.</b> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in leo id dui bibendum fringilla in et arcu. In vehicula vel est sed mattis. Duis varius, sem non mattis.</div>
 
 </body>
@@ -26,7 +29,7 @@ class Home extends StatelessWidget {
       context,
       htmlContent,
       linksCallback: (dynamic link) {
-        debugPrint('You clicked on ${link.toString()}');
+        debugPrint('You clicked on $link');
       },
       // as name suggests, optionally set the default text style
       defaultTextStyle: TextStyle(color: Colors.grey[700]),
@@ -42,10 +45,12 @@ class Home extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(title: const Text('Demo')),
-        body: Container(
-          color: Colors.white,
-          padding: const EdgeInsets.all(16.0),
-          child: RichText(text: textSpan),
+        body: SingleChildScrollView(
+          child: Container(
+            color: Colors.white,
+            padding: const EdgeInsets.all(16.0),
+            child: RichText(text: textSpan),
+          ),
         ),
       ),
     );
